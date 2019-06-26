4bd3d9600e621304ac9f0facc3487ba698f79c2a
ItzPikaExploits <38090954+ItzPikaExploits@users.noreply.github.com>
2019-06-26 15:20:14 -0700

    Update sights.js

--- sights.js
+++ sights.js
@@ -49,44 +49,7 @@ client.on("message", (message) => {
     };
     if (msg.startsWith(prefix + "r34")) {
         editedmessage = message.content.slice(prefix.length + 4)
-        var url = 'https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=' + editedmessage;
- 
-        https.get(url, function(res){
-            var body = '';
-   
-            res.on('data', function(chunk){
-                body += chunk;
-            });
-   
-            res.on('end', function(){
-                var parser = new xml2js.Parser();
-                parser.parseString(body, function (err, result) {
-                    var postCount = result.posts.$.count - 1;
-                    if(postCount > 100) {
-                        postCount = 100;
-                    }
-                    if(postCount > 0) {
-                        var picNum = Math.floor(Math.random() * postCount) + 0;
-                        var r34Pic = result.posts.post[picNum].$.file_url;
-                        // console.log(result.posts.post[picNum].$.file_url);
-                        let embed = new discord.RichEmbed()
-                            .setColor("LUMINOUS_VIVID_PINK")
-                            .setTitle("Rule34: " + editedmessage)
-                            .setFooter(result.posts.post[picNum].$.file_url)
-                            .setImage(r34Pic)
-                            .setDescription("Enjoy~!")
-                        message.channel.send(embed)
-                   
-                    } else {
-                        console.log("Nothing found:", editedmessage);
-                        message.channel.send(":regional_indicator_x: Nobody here but us chickens!");
-                    }
-
-                    });
-                });
-            }).on('error', function(e){
-                console.log("Got an error: ", e);
-        });
+        require('./booru').fn(msg, 'rule34 ' + editedmessage)
     };
     // Emoji Commands
     if (msg.startsWith("👀")) {
