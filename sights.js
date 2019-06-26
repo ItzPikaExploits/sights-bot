44625ac34fd87f2a473cf93d8105733e2dd68e3e
ItzPikaExploits <38090954+ItzPikaExploits@users.noreply.github.com>
2019-06-26 15:53:25 -0700

    Update sights.js

--- sights.js
+++ sights.js
@@ -89,44 +89,50 @@ client.on("message", (message) => {
         });
     };
     if (msg.startsWith(prefix + "e621")) {
-        editedmessage = message.content.slice(prefix.length + 5)
-        var url = 'https://e621.net/post/index.json?tags=order:random+' + editedmessage;
- 
-        https.get(url, function(res){
-            var body = '';
-   
-            res.on('data', function(chunk){
-                body += chunk;
-            });
-   
-            res.on('end', function(){
-                var parser = new xml2js.Parser();
-                parser.parseString(body, function (err, result) {
-                    var postCount = result.posts.$.count - 1;
-                    if(postCount > 100) {
-                        postCount = 100;
-                    }
-                    if(postCount > 0) {
-                        var picNum = Math.floor(Math.random() * postCount) + 0;
-                        var e621Pic = estoThing[0].file_url.toString();
-                        console.log(e621Pic);
-                        let embed = new discord.RichEmbed()
-                            .setColor("LUMINOUS_VIVID_PINK")
-                            .setTitle("E621: " + editedmessage)
-                            .setFooter(e621Pic)
-                            .setImage(e621Pic)
-                            .setDescription("Enjoy~! OwO")
-                        message.channel.send(embed)
-                   
-                    } else {
-                        console.log("Nothing found:", editedmessage);
-                        message.channel.send(":regional_indicator_x: Nobody here but us chickens!");
-                    }
-
-                    });
-                });
-            }).on('error', function(e){
-                console.log("Got an error: ", e);
+        params = message.content.slice(prefix.length + 5)
+        var tagesto = "";
+        var tagestosplit = params.split(",");
+        for (var i = 0; i < tagestosplit.length; i++) {
+            tagestosplit[i] = tagestosplit[i].trim();
+            tagestosplit[i] = tagestosplit[i].replace(/\s/g, "_");
+            if(estoBanList.indexOf(tagestosplit[i]) != -1){
+                message.channel.send(":regional_indicator_x: No. Stop it.");
+                return;
+            }
+        }
+        tagesto = tagestosplit.join("+");
+        if (message.channel.type === "dm" || message.channel.name.indexOf("the_art_gallery") != -1 || message.channel.name.indexOf("furry") != -1 || message.channel.name.indexOf("2am") != -1 || message.channel.name.indexOf("nsfw") != -1) {
+            console.log("Safe to post NSFW content.");
+        }
+        else {
+            tagesto += "+rating:safe";
+            if ((tagesto.indexOf("rating:explicit") != -1) || (tagesto.indexOf("penis") != -1) || (tagesto.indexOf("pussy") != -1) || (tagesto.indexOf("anus") != -1) || (tagesto.indexOf("dick") != -1) || tagesto.indexOf("rating:questionable") != -1 || tagesto.indexOf("genitalia") != -1 || tagesto.indexOf("genitals") != -1 || tagesto.indexOf("genital") != -1 || tagesto.indexOf("vagina") != -1 || tagesto.indexOf("cunt") != -1 || tagesto.indexOf("vaginal") != -1 || tagesto.indexOf("vaginal_penetration") != -1 || tagesto.indexOf("sex") != -1 || tagesto.indexOf("fuck") != -1 || tagesto.indexOf("intercourse") != -1 || tagesto.indexOf("cock") != -1) {
+                message.channel.send(":regional_indicator_x: That content isn't appropiate for this channel. Go be naughty elsewhere.", {files:[{attachment: __dirname + bruh.jpg}]});
+                return;
+            }
+        }
+        var estoHeader = {
+            url: 'https://e621.net/post/index.json?tags=order:random+' + tagesto,
+            headers: {
+                'User-Agent': 'Yoshi-Bot/${process.version} (by NeoNinetales on e621)'
+            }
+        }
+        request(estoHeader,
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var estoThing = JSON.parse(body);
+                if (typeof (estoThing[0]) != "undefined") {
+                    let embed = new discord.RichEmbed()
+                        .setColor("LUMINOUS_VIVID_PINK")
+                        .setTitle("E621: " + editedmessage)
+                        .setFooter(estoThing[0].file_url.toString())
+                        .setImage("https://e621.net/post/show/" + estoThing[0].id.toString())
+                        .setDescription("Enjoy~! OwO")
+                }
+                else {
+                    msg.channel.send("No images found. Try different tags.")
+                }
+            }
         });
     };
     // Emoji Commands
